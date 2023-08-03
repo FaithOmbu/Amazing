8ca1f2c8e20158693a0dbceaa7f4aec658d0e110
FaithOmbu <141145267+FaithOmbu@users.noreply.github.com>
2023-08-03 12:16:27 +0100

    Add files via upload
    African Food

---
+++ index.js
@@ -0,0 +1,13 @@
+function sayOriginisBeninRepublic() {
+  document.getElementById("divOne").innerHTML =
+    "Origin of Ewa Agoyin is Benin Republic";
+}
+
+/*mouseover & mouseout events*/
+function setNewImage() {
+  document.getElementById("img1").src = "Ewa-G.jpg";
+}
+function setOldImage() {
+  document.getElementById("img1").src = "agoyin.png";
+  //alert();
+}
